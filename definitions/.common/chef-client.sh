92c0f2213f03bf00e18af5f531ce511489223374
Patrick Debois <Patrick.Debois@jedi.be>
2013-02-07 13:19:08 +0100

    avoid chef v11 installation, resort to last chef v10

--- definitions/.common/chef-client.sh
+++ definitions/.common/chef-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eux
 
-wget -O - http://opscode.com/chef/install.sh | sudo bash
+wget -O - http://opscode.com/chef/install.sh | sudo bash -s -- -v 10.18.2
+#gem install chef --version 0.16.4 --no-ri --no-rdocs
